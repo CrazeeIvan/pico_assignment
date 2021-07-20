5f3e5ca96d649c505202195018916b2f99ae3cde
Cloud User <ec2-user@ip-172-31-39-226.eu-west-1.compute.internal>
2021-07-20 15:48:10 +0000

    Workable prototype for json building.

--- test.py
+++ test.py
@@ -4,14 +4,15 @@ import xmltodict
 with open ("dataset1/0001/single_case.xml") as xml_file:
 	data_dict = xmltodict.parse(xml_file.read())
 xml_file.close()
-print(data_dict['result'])
-data_dict['result']['@plugin']="hello"
-print(data_dict['result'])
-json_data = json.dumps(data_dict)
+#print(data_dict['result'])
+#data_dict['result']['@plugin']="hello"
+#print(data_dict['result'])
+a = '{'
+a += '"file" : "' + data_dict['result']['suites']['suite']['file'] + '"'
+a += '}'
+n = json.loads(a)
+json_data = json.dumps(n)
 with open ("data.json", "w") as json_file:
 	json_file.write(json_data)
 json_file.close()
-a = "{" + "\"" + "file" + "\"" + ": \"" + data_dict['result']['suites']['suite']['file'] + "\"" + "}"
-print(a)
-
 
