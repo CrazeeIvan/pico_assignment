c6d92317fed680a7421c1e502131f1c023a7a808
Cloud User <ec2-user@ip-172-31-39-226.eu-west-1.compute.internal>
2021-07-20 15:19:43 +0000

    added simple json builder test.

--- test.py
+++ test.py
@@ -11,4 +11,7 @@ json_data = json.dumps(data_dict)
 with open ("data.json", "w") as json_file:
 	json_file.write(json_data)
 json_file.close()
+a = "{" + "\"" + "file" + "\"" + ": \"" + data_dict['result']['suites']['suite']['file'] + "\"" + "}"
+print(a)
+
 
