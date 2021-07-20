a42bbb9adf8c276cd2419d7cae121d70cee2da5d
Cloud User <ec2-user@ip-172-31-39-226.eu-west-1.compute.internal>
2021-07-20 16:41:20 +0000

    Updated to use dict for json building instead of string concatenation.

--- test.py
+++ test.py
@@ -4,24 +4,23 @@ import xmltodict
 with open ("dataset1/0001/single_case.xml") as xml_file:
 	data_dict = xmltodict.parse(xml_file.read())
 xml_file.close()
-a = '{'
-a += '"file" : "' + data_dict['result']['suites']['suite']['file'] + '"'
-a += '"name" : "' + data_dict['result']['suites']['suite']['name'] + '"'
-a += '"timestamp" : "' + data_dict['result']['suites']['suite']['timestamp'] + '"'
-a += '"classname" : "' + data_dict['result']['suites']['suite']['cases']['case']['classname'] + '"'
-a += '"testname" : "' + data_dict['result']['suites']['suite']['cases']['case']['testname'] + '"'
-a += '"duration" : "' + data_dict['result']['suites']['suite']['cases']['case']['duration'] + '"'
-a += '"skipped" : "' + data_dict['result']['suites']['suite']['cases']['case']['skipped'] + '"'
-a += '"passed" : "true"'
-a += '"starttime" : "' + data_dict['result']['suites']['suite']['cases']['case']['classname'] + '"'
-a += '"stdout" : "' + data_dict['result']['suites']['suite']['stdout'] + '"'
-a += '"stderr" : "' + data_dict['result']['suites']['suite']['stderr'] + '"'
+n = {}
+n['file'] = data_dict['result']['suites']['suite']['file']
+n['name'] = data_dict['result']['suites']['suite']['name']
+n['timestamp'] = data_dict['result']['suites']['suite']['timestamp']
+n['classname'] = data_dict['result']['suites']['suite']['cases']['case']['className']
+n['testname'] = data_dict['result']['suites']['suite']['cases']['case']['testName']
+n['duration'] = data_dict['result']['suites']['suite']['cases']['case']['duration']
+n['skipped'] = data_dict['result']['suites']['suite']['cases']['case']['skipped']
+n['passed'] = 'false' if data_dict['result']['suites']['suite']['cases']['case']['failedSince'] != 0 else 'true'
+n['starttime'] = data_dict['result']['suites']['suite']['cases']['case']['className']
+n['stdout'] = data_dict['result']['suites']['suite']['stdout']
+n['stderr'] = data_dict['result']['suites']['suite']['stderr']
 #Generate ID based on <name>_<testname>_<timestamp>
-a += '"id" : "' + data_dict['result']['suites']['suite']['name'] + '"'
-
-a += '}'
-n = json.loads(a)
+n['id'] = data_dict['result']['suites']['suite']['name']
+print(n)
 json_data = json.dumps(n)
 with open ("data.json", "w") as json_file:
 	json_file.write(json_data)
 json_file.close()
+
